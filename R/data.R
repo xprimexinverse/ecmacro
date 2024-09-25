7aa189ab3636db4ff89ffe8078eab7b743ebcb26
Graeme Walsh <graeme.walsh@hotmail.co.uk>
2024-09-25 19:34:05 +0100

    fixed typo

--- R/data.R
+++ R/data.R
@@ -5,8 +5,8 @@
 #' @format A data frame (32637 obs of 71 variables):
 #' \describe{
 #' \item{CODE}{Code}
-#' \item{COUNTRY}{Country}}
-#' \item{SUB_CHAPTER}{Sub-chapter}}
-#' \item{TITLE}{Title}}
+#' \item{COUNTRY}{Country}
+#' \item{SUB_CHAPTER}{Sub-chapter}
+#' \item{TITLE}{Title}
 #' \item{UNIT}{Unit}}
 "AMECO_spring_2024"
